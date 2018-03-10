a02f8e3a823ae41e8488c6ab4fcdfffd65d7c894
fairmarketcoin <matt.a.churchwell@gmail.com>
2018-03-09 20:33:54 -0600

    Money Supply
    Correction

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -34,7 +34,7 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
 // MONEY_SUPPLY - total number coins to be generated
-const uint64_t MONEY_SUPPLY                                  = 18446744073709551615;
+const uint64_t MONEY_SUPPLY                                  = static_cast<uint64_t>(-1);
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
